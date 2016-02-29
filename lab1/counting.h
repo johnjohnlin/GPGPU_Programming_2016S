281d2d8138f50c8d612ba589eab403c2d76d102c
johnjohnlin <johnjohnlin@gmail.com>
2016-02-29 23:03:53 +0800

    Update lab1

---
+++ lab1/counting.h
@@ -0,0 +1,4 @@
+#pragma once
+void CountPosition(const char *text, int *pos, int text_size);
+int ExtractHead(const int *pos, int *head, int text_size);
+void Part3(char *text, int *pos, int *head, int text_size, int n_head);
