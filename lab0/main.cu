0541e7dd0c11f1cf638dad2ea1a3dcb3695447ee
ChangNingNing <r05922087@ntu.edu.tw>
2017-02-24 14:06:14 +0800

    r05922087_lab0 hw

--- lab0/main.cu
+++ lab0/main.cu
@@ -11,13 +11,14 @@
 }
 
 const int W = 40;
-const int H = 12;
+const int H = 16;
 
 __global__ void Draw(char *frame) {
 	// TODO: draw more complex things here
 	// Do not just submit the original file provided by the TA!
 	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 	const int x = blockIdx.x * blockDim.x + threadIdx.x;
+	long long int bit[16] = {	0, 16252928, 9175040, 8781824, 8585216, 8486912, 2139120636, 1073757952, 1073742844, 1073742844, 1073742844, 1073742844, 1073742844, 1073774588, 1073710076, 0};
 	if (y < H and x < W) {
 		char c;
 		if (x == W-1) {
@@ -25,7 +26,10 @@ __global__ void Draw(char *frame) {
 		} else if (y == 0 or y == H-1 or x == 0 or x == W-2) {
 			c = ':';
 		} else {
-			c = ' ';
+			if((bit[y]>>(x%64))&1)
+				c = '=';
+			else
+				c = ' ';
 		}
 		frame[y*W+x] = c;
 	}
